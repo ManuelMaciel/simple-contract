2bc6933f6e9d79ac5a4f51591cb01cca6a17206e
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-12-09 23:39:32 -0300

    feat: add contract and functions

---
+++ Inbox.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.17;
+
+
+contract Inbox {
+    string public message;
+
+    function inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessages() public view returns(string) {
+        return message;
+    }
+}
